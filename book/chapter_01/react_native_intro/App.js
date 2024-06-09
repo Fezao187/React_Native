0f1c16aed9d46a319a6c431bdbba91496d5ed69f
Katlego Maredi <katlegomaredi9@gmail.com>
2024-06-09 15:30:26 +0200

    Chapter 1: Introduction to react Native

--- book/chapter_01/react_native_intro/App.js
+++ book/chapter_01/react_native_intro/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Learn React Native</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    fontWeight: "bold",
   },
 });
